0e08009c3b1eb662aad5e612e2c0fe0a4b1806ad
belazr <72708147+belazr@users.noreply.github.com>
2023-03-10 21:16:53 +0100

    Added setWindowsHook to code launching functions

--- src/launch.cpp
+++ src/launch.cpp
@@ -52,9 +52,9 @@ namespace launch {
 	// pushf
 	// movabs rax, pFunc
 	// movabs rcx, pArg
-	// sub    rsp, 0x28						setup shadow space for function call
+	// sub    rsp, 0x20						setup shadow space for function call
 	// call   rax							call pFunc
-	// add    rsp, 0x28						cleanup shadow space from function call
+	// add    rsp, 0x20						cleanup shadow space from function call
 	// lea    rcx, [rip - 0x3F]				write return value to beginning of shell code
 	// mov    QWORD PTR[rcx], rax
 	// popf
@@ -67,7 +67,64 @@ namespace launch {
 	// pop    rax
 	// mov    BYTE PTR[rip + 0x01], 0x01	set flag to one
 	// ret									return to old rip
-	static BYTE hijackShellX64[]{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x35, 0xF2, 0xFF, 0xFF, 0xFF, 0x50, 0x51, 0x52, 0x41, 0x50, 0x41, 0x51, 0x41, 0x52, 0x41, 0x53, 0x9C, 0x48, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0xB9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x83, 0xEC, 0x28, 0xFF, 0xD0, 0x48, 0x83, 0xC4, 0x28, 0x48, 0x8D, 0x0D, 0xC1, 0xFF, 0xFF, 0xFF, 0x48, 0x89, 0x01, 0x9D, 0x41, 0x5B, 0x41, 0x5A, 0x41, 0x59, 0x41, 0x58, 0x5A, 0x59, 0x58, 0xC6, 0x05, 0x01, 0x00, 0x00, 0x00, 0x01, 0xC3, 0x00 };
+	static BYTE hijackShellX64[]{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x35, 0xF2, 0xFF, 0xFF, 0xFF, 0x50, 0x51, 0x52, 0x41, 0x50, 0x41, 0x51, 0x41, 0x52, 0x41, 0x53, 0x9C, 0x48, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0xB9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x83, 0xEC, 0x20, 0xFF, 0xD0, 0x48, 0x83, 0xC4, 0x20, 0x48, 0x8D, 0x0D, 0xC1, 0xFF, 0xFF, 0xFF, 0x48, 0x89, 0x01, 0x9D, 0x41, 0x5B, 0x41, 0x5A, 0x41, 0x59, 0x41, 0x58, 0x5A, 0x59, 0x58, 0xC6, 0x05, 0x01, 0x00, 0x00, 0x00, 0x01, 0xC3, 0x00 };
+
+	#ifdef _WIN64
+
+	// ASM:
+	// push   rbp							save registers
+	// push   rsp
+	// push   rbx
+	// push   r8							save parameters for CallNextHookEx
+	// push   rdx
+	// push   rcx
+	// lea    rbx, [rip - 0x1E]				load shell code base to rbx
+	// jmp    0x0							jump to skip pFunc call after first execution
+	// mov    BYTE PTR[rip - 0x0B], 0x1E	fix jump to skip pFunc call
+	// movabs rcx, pArg						
+	// sub    rsp, 0x28						setup shadow space for function call
+	// call   pFunc
+	// add    rsp, 0x28						cleanup shadow space from function call
+	// mov    QWORD PTR[rbx], rax			write return to beginning of shell code
+	// pop    rdx							set up call of CallNextHookEx
+	// pop    r8
+	// pop    r9
+	// sub    rsp, 0x28						setup shadow space for function call
+	// call   QWORD PTR [rbx + 0x8]			call CallNextHookEx
+	// add    rsp, 0x28						cleanup shadow space from function call
+	// pop    rbx
+	// pop    rsp
+	// pop    rbp
+	// mov    BYTE PTR[rip + 0x1], 0x1		set flag to one
+	// ret
+	static BYTE windowsHookShell[]{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x55, 0x54, 0x53, 0x41, 0x50, 0x52, 0x51, 0x48, 0x8D, 0x1D, 0xE2, 0xFF, 0xFF, 0xFF, 0xE9, 0x00, 0x00, 0x00, 0x00, 0xC6, 0x05, 0xF5, 0xFF, 0xFF, 0xFF, 0x1E, 0x48, 0xB9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x83, 0xEC, 0x28, 0xFF, 0x13, 0x48, 0x83, 0xC4, 0x28, 0x48, 0x89, 0x03, 0x5A, 0x41, 0x58, 0x41, 0x59, 0x48, 0x83, 0xEC, 0x28, 0xFF, 0x53, 0x08, 0x48, 0x83, 0xC4, 0x28, 0x5B, 0x5C, 0x5D, 0xC6, 0x05, 0x01, 0x00, 0x00, 0x00, 0x01, 0xC3, 0x00 };
+
+	#else
+
+	// ASM:
+	// push   ebp							set up stack frame
+	// mov    ebp, esp
+	// jmp    0x0							jump to skip pFunc call after first execution
+	// push   eax
+	// push   ebx
+	// mov    ebx, pShellCode
+	// mov    BYTE PTR[ebx + 0x08], 0x19	fix jump to skip pFunc call
+	// push   pArg
+	// call   pFunc
+	// mov    DWORD PTR[ebx], eax			write return to beginning of shell code
+	// pop    ebx
+	// pop    eax
+	// push   DWORD PTR[ebp + 0x10]			set up call of CallNextHookEx
+	// push   DWORD PTR[ebp + 0xc]
+	// push   DWORD PTR[ebp + 0x8]
+	// push   0x0
+	// call   pCallNextHookEx
+	// pop    ebp
+	// mov    BYTE PTR ds:pFlag, 0x1		set flag to one
+	// ret    0xc
+	static BYTE windowsHookShell[]{ 0x00, 0x00, 0x00, 0x00, 0x55, 0x89, 0xE5, 0xEB, 0x00, 0x50, 0x53, 0xBB, 0x00, 0x00, 0x00, 0x00, 0xC6, 0x43, 0x08, 0x19, 0x68, 0x00, 0x00, 0x00, 0x00, 0xE8, 0x00, 0x00, 0x00, 0x00, 0x89, 0x03, 0x5B, 0x58, 0xFF, 0x75, 0x10, 0xFF, 0x75, 0x0C, 0xFF, 0x75, 0x08, 0x6A, 0x00, 0xE8, 0x00, 0x00, 0x00, 0x00, 0x5D, 0xC6, 0x05, 0x00, 0x00, 0x00, 0x00, 0x01, 0xC2, 0x0C, 0x00, 0x00 };
+
+	#endif
 
 
 	bool createRemoteThread(HANDLE hProc, tLaunchFunc pFunc, void* pArg, void** pRet) {
@@ -176,7 +233,6 @@ namespace launch {
 
 		if (!hThread) return false;
 
-		// thread can not be suspended
 		if (SuspendThread(hThread) == 0xFFFFFFFF) {
 			CloseHandle(hThread);
 
@@ -236,7 +292,6 @@ namespace launch {
 			// post thread message to ensure thread execution
 			PostThreadMessageA(threadEntry.th32ThreadID, 0, 0, 0);
 
-			// thread can not be resumed
 			if (ResumeThread(hThread) == 0xFFFFFFFF) {
 				wow64Context.Eip = oldEip;
 				Wow64SetThreadContext(hThread, &wow64Context);
@@ -287,7 +342,7 @@ namespace launch {
 			const uint64_t oldRip = context.Rip;
 			// set new instruction pointer to beginning of shell code function
 			context.Rip = reinterpret_cast<uint64_t>(pShellCode + 0x08);
-			// offset of flag in the shell code to signal successfull execution
+			// flag in the shell code to signal successfull execution
 			const BYTE* const pFlag = pShellCode + sizeof(hijackShellX64) - 0x01;
 
 			*reinterpret_cast<uint64_t*>(hijackShellX64 + 0x00) = oldRip;
@@ -313,7 +368,6 @@ namespace launch {
 			// post thread message to ensure thread execution
 			PostThreadMessageA(threadEntry.th32ThreadID, 0, 0, 0);
 
-			// thread can not be resumed
 			if (ResumeThread(hThread) == 0xFFFFFFFF) {
 				context.Rip = oldRip;
 				SetThreadContext(hThread, &context);
@@ -356,6 +410,115 @@ namespace launch {
 	}
 
 
+	typedef struct HookData {
+		DWORD procId;
+		HMODULE hModule;
+		HOOKPROC pHookFunc;
+		HHOOK hHook;
+		HWND hWnd;
+	}HookData;
+
+	static BOOL CALLBACK setHookCallback(HWND hWnd, LPARAM pArg);
+
+	bool setWindowsHook(HANDLE hProc, tLaunchFunc pFunc, void* pArg, void** pRet) {
+		BOOL isWow64 = FALSE;
+		IsWow64Process(hProc, &isWow64);
+
+		// installing hook only possible from process with matching architechture
+		#ifdef _WIN64
+
+		if (isWow64) return false;
+
+		#else
+
+		if (!isWow64) return false;
+
+		#endif // _WIN64
+
+		const HMODULE hUser32 = proc::ex::getModuleHandle(hProc, "User32.dll");
+
+		if (!hUser32) return false;
+
+		const FARPROC pCallNextHookEx = proc::ex::getProcAddress(hProc, hUser32, "CallNextHookEx");
+
+		if (!pCallNextHookEx) return false;
+
+		BYTE* const pShellCode = reinterpret_cast<BYTE*>(VirtualAllocEx(hProc, nullptr, sizeof(windowsHookShell), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE));
+
+		if (!pShellCode) return false;
+
+		// flag in the shell code to signal successfull execution
+		const BYTE* const pFlag = pShellCode + sizeof(windowsHookShell) - 0x01;
+
+		HookData hookData{};
+		hookData.procId = GetProcessId(hProc);
+		// arbitrary but has to be loaded in both caller and target process
+		hookData.hModule = proc::in::getModuleHandle("Kernel32.dll");
+
+		#ifdef _WIN64
+
+		hookData.pHookFunc = reinterpret_cast<HOOKPROC>(pShellCode + 0x10);
+
+		*reinterpret_cast<uint64_t*>(windowsHookShell) = reinterpret_cast<uint64_t>(pFunc);
+		*reinterpret_cast<uint64_t*>(windowsHookShell + 0x08) = reinterpret_cast<uint64_t>(pCallNextHookEx);
+		*reinterpret_cast<uint64_t*>(windowsHookShell + 0x2C) = reinterpret_cast<uint64_t>(pArg);
+
+		#else
+
+		hookData.pHookFunc = reinterpret_cast<HOOKPROC>(pShellCode + 0x04);
+
+		*reinterpret_cast<uint32_t*>(windowsHookShell + 0x0C) = reinterpret_cast<uint32_t>(pShellCode);
+		*reinterpret_cast<uint32_t*>(windowsHookShell + 0x15) = reinterpret_cast<uint32_t>(pArg);
+		*reinterpret_cast<uint32_t*>(windowsHookShell + 0x1A) = reinterpret_cast<uint32_t>(pFunc) - (reinterpret_cast<uint32_t>(pShellCode) + 0x1E);
+		*reinterpret_cast<uint32_t*>(windowsHookShell + 0x2E) = reinterpret_cast<uint32_t>(pCallNextHookEx) - (reinterpret_cast<uint32_t>(pShellCode) + 0x32);
+		*reinterpret_cast<uint32_t*>(windowsHookShell + 0x35) = reinterpret_cast<uint32_t>(pFlag);
+
+		#endif // _WIN64
+
+		if (!WriteProcessMemory(hProc, pShellCode, windowsHookShell, sizeof(windowsHookShell), nullptr)) {
+			VirtualFreeEx(hProc, pShellCode, 0, MEM_RELEASE);
+
+			return false;
+		}
+
+		if (!EnumWindows(setHookCallback, reinterpret_cast<LPARAM>(&hookData))) {
+			VirtualFreeEx(hProc, pShellCode, 0, MEM_RELEASE);
+
+			return false;
+		}
+
+		if (!hookData.hHook || !hookData.hWnd) {
+			VirtualFreeEx(hProc, pShellCode, 0, MEM_RELEASE);
+
+			return false;
+		}
+
+		// foreground window to activate the hook
+		const HWND hFgWnd = GetForegroundWindow();
+		SetForegroundWindow(hookData.hWnd);
+		SetForegroundWindow(hFgWnd);
+
+		if (!checkShellCodeFlag(hProc, pFlag)) {
+			UnhookWindowsHookEx(hookData.hHook);
+			VirtualFreeEx(hProc, pShellCode, 0, MEM_RELEASE);
+
+			return false;
+		}
+
+		UnhookWindowsHookEx(hookData.hHook);
+
+		if (!ReadProcessMemory(hProc, pShellCode, pRet, sizeof(uintptr_t), nullptr)) {
+			VirtualFreeEx(hProc, pShellCode, 0, MEM_RELEASE);
+
+			return false;
+		}
+
+		VirtualFreeEx(hProc, pShellCode, 0, MEM_RELEASE);
+
+		return true;
+	}
+
+
 	static bool checkShellCodeFlag(HANDLE hProc, const BYTE* pFlag) {
 		bool flag = false;
 		ULONGLONG start = GetTickCount64();
@@ -369,6 +532,35 @@ namespace launch {
 		} while (GetTickCount64() - start < THREAD_TIMEOUT);
 
 		return flag;
+	}
+
+
+	// set hook in first possible window thread
+	static BOOL CALLBACK setHookCallback(HWND hWnd, LPARAM pArg) {
+		HookData* const pHookCallbackData = reinterpret_cast<HookData*>(pArg);
+		DWORD curProcId = 0;
+		const DWORD curThreadId = GetWindowThreadProcessId(hWnd, &curProcId);
+
+		if (!curThreadId || !curProcId) return TRUE;
+
+		if (!pHookCallbackData->hHook && curProcId == pHookCallbackData->procId) {
+			char className[MAX_PATH]{};
+
+			// window procedures hooks only possible for non-console windows
+			if (IsWindowVisible(hWnd) && GetClassNameA(hWnd, className, MAX_PATH) && strcmp(className, "ConsoleWindowClass")) {
+				// pretend the shell code is loaded by the module
+				HHOOK hHook = SetWindowsHookExA(WH_CALLWNDPROC, pHookCallbackData->pHookFunc, pHookCallbackData->hModule, curThreadId);
+
+				if (hHook) {
+					pHookCallbackData->hHook = hHook;
+					pHookCallbackData->hWnd = hWnd;
+				}
+
+			}
+
+		}
+
+		return TRUE;
 	}
 
 }
