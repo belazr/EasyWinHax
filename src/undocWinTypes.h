0f190eeb9775907c56cc634fd4466719d00f8513
belazr <72708147+belazr@users.noreply.github.com>
2023-04-16 20:45:40 +0200

    Added getDuplicateProcessHandle

--- src/undocWinTypes.h
+++ src/undocWinTypes.h
@@ -760,6 +760,21 @@ typedef struct _SYSTEM_PROCESS_INFORMATION {
     SYSTEM_THREAD_INFORMATION Threads[1];
 } SYSTEM_PROCESS_INFORMATION, * PSYSTEM_PROCESS_INFORMATION;
 
+typedef struct _SYSTEM_HANDLE_TABLE_ENTRY_INFO {
+    USHORT UniqueProcessId;
+    USHORT CreatorBackTraceIndex;
+    UCHAR ObjectTypeIndex;
+    UCHAR HandleAttributes;
+    USHORT HandleValue;
+    PVOID Object;
+    ULONG GrantedAccess;
+} SYSTEM_HANDLE_TABLE_ENTRY_INFO, * PSYSTEM_HANDLE_TABLE_ENTRY_INFO;
+
+typedef struct _SYSTEM_HANDLE_INFORMATION {
+    ULONG NumberOfHandles;
+    SYSTEM_HANDLE_TABLE_ENTRY_INFO Handles[1];
+} SYSTEM_HANDLE_INFORMATION, * PSYSTEM_HANDLE_INFORMATION;
+
 typedef NTSTATUS(__stdcall* tNtQuerySystemInformation) (
     SYSTEM_INFORMATION_CLASS SystemInformationClass,
     PVOID SystemInformation,
