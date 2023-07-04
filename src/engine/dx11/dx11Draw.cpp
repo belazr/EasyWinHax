0b2d7bae78b3b7323131738988feaacda60378ed
belazr <72708147+belazr@users.noreply.github.com>
2023-07-04 23:32:20 +0200

    WIP Triangle strip drawing with dx11

--- src/engine/dx11/dx11Draw.cpp
+++ src/engine/dx11/dx11Draw.cpp
@@ -38,16 +38,51 @@ namespace hax {
 
 
 		Draw::Draw() :
-			_pSwapChain(nullptr), _pDevice(nullptr), _pContext(nullptr), _pRenderTargetView(nullptr), _pVertexShader(nullptr),
-			_pVertexLayout(nullptr), _pPixelShader(nullptr), _pConstantBuffer(nullptr), _viewport{}
+			_pDevice(nullptr), _pContext(nullptr), _pVertexShader(nullptr), _pVertexLayout(nullptr), _pPixelShader(nullptr), _pConstantBuffer(nullptr), _originalTopology{}
 		{}
 
 
+		Draw::~Draw() {
+			ULONG ret = 0;
+
+			if (this->_pDevice) {
+				ret = this->_pDevice->Release();
+			}
+
+			if (this->_pContext) {
+				ret = this->_pContext->Release();
+			}
+
+			if (this->_pVertexShader) {
+				ret = this->_pVertexShader->Release();
+			}
+
+			if (this->_pVertexLayout) {
+				ret = this->_pVertexLayout->Release();
+			}
+
+			if (this->_pDevice) {
+				ret = this->_pDevice->Release();
+			}
+
+			if (this->_pPixelShader) {
+				ret = this->_pPixelShader->Release();
+			}
+
+			if (this->_pConstantBuffer) {
+				ret = this->_pConstantBuffer->Release();
+			}
+
+		}
+
+
+		static BOOL CALLBACK getMainWindowCallback(HWND hWnd, LPARAM lParam);
+
 		void Draw::beginDraw(const Engine* pEngine) {
 
-			if (!this->_pSwapChain) {
-				this->_pSwapChain = reinterpret_cast<IDXGISwapChain*>(const_cast<hax::Engine*>(pEngine));
-				const HRESULT hResult = this->_pSwapChain->GetDevice(__uuidof(ID3D11Device), reinterpret_cast<void**>(&this->_pDevice));
+			if (!this->_originalTopology) {
+				IDXGISwapChain* const pSwapChain = reinterpret_cast<IDXGISwapChain*>(const_cast<hax::Engine*>(pEngine));
+				const HRESULT hResult = pSwapChain->GetDevice(__uuidof(ID3D11Device), reinterpret_cast<void**>(&this->_pDevice));
 
 				if (hResult != S_OK || !this->_pDevice) return;
 
@@ -55,97 +90,127 @@ namespace hax {
 
 				if (!this->_pContext) return;
 
-				this->_pContext->OMGetRenderTargets(1, &this->_pRenderTargetView, nullptr);
+				D3D11_VIEWPORT viewports[D3D11_VIEWPORT_AND_SCISSORRECT_OBJECT_COUNT_PER_PIPELINE]{};
+				UINT viewportCount = sizeof(viewports) / sizeof(D3D11_VIEWPORT);
 
-				if (!this->_pRenderTargetView) {
-					this->_pRenderTargetView = createRenderTargetView();
-					this->_pContext->OMSetRenderTargets(1, &this->_pRenderTargetView, nullptr);
+				this->_pContext->RSGetViewports(&viewportCount, viewports);
+
+				D3D11_VIEWPORT viewport = viewports[0];
+
+				if (!viewportCount || !viewport.Width) {
+					HWND hMainWnd = nullptr;
+
+					EnumWindows(getMainWindowCallback, reinterpret_cast<LPARAM>(&hMainWnd));
+
+					if (!hMainWnd) return;
+
+					RECT windowRect{};
+
+					if (!GetClientRect(hMainWnd, &windowRect)) return;
+
+					viewport.Width = static_cast<FLOAT>(windowRect.right);
+					viewport.Height = static_cast<FLOAT>(windowRect.bottom);
+					viewport.TopLeftX = static_cast<FLOAT>(windowRect.left);
+					viewport.TopLeftY = static_cast<FLOAT>(windowRect.top);
+					viewport.MinDepth = 0.0f;
+					viewport.MaxDepth = 1.0f;
+					this->_pContext->RSSetViewports(1, &viewport);
 				}
 
 				if (!this->compileShaders()) return;
-				if (!this->getViewport()) return;
-				if (!this->setupOrtho()) return;
+
+				if (!this->setupOrtho(viewport.Width, viewport.Height)) return;
+
+				this->_pContext->IAGetPrimitiveTopology(&this->_originalTopology);
 
 			}
 
+			this->_pContext->VSSetShader(this->_pVertexShader, nullptr, 0);
+			this->_pContext->IASetInputLayout(this->_pVertexLayout);
+			this->_pContext->PSSetShader(this->_pPixelShader, nullptr, 0);
+			this->_pContext->VSSetConstantBuffers(0, 1, &this->_pConstantBuffer);
+
 			return;
+		}
+
+
+		static BOOL CALLBACK getMainWindowCallback(HWND hWnd, LPARAM lParam) {
+			DWORD processId = 0;
+			GetWindowThreadProcessId(hWnd, &processId);
+
+			if (!processId || GetCurrentProcessId() != processId || GetWindow(hWnd, GW_OWNER) || !IsWindowVisible(hWnd)) return TRUE;
+
+			*reinterpret_cast<HWND*>(lParam) = hWnd;
+
+			return FALSE;
 		}
 
 
 		void Draw::endDraw(const Engine* pEngine) const {
 			UNREFERENCED_PARAMETER(pEngine);
 
+			this->_pContext->IASetPrimitiveTopology(this->_originalTopology);
+
 			return;
 		}
 
 
-		typedef struct Vertex{
-			DirectX::XMFLOAT3 pos;
+		typedef struct Vertex {
+			float x, y, z;
 			D3DCOLORVALUE color;
 		}Vertex;
 
 		void Draw::drawTriangleStrip(const Vector2 corners[], UINT count, rgb::Color color) const {
-			D3D11_BUFFER_DESC bufferDescLine{ 0 };
+
+			if (!this->_pDevice || !this->_pContext) return;
+
+			Vertex* const data = new Vertex[count];
+
+			if (!data) return;
+
+			for (UINT i = 0; i < count; i++) {
+				data[i].x = corners[i].x;
+				data[i].y = corners[i].y;
+				data[i].z = 1.f;
+				data[i].color.r = FLOAT_R(color);
+				data[i].color.g = FLOAT_G(color);
+				data[i].color.b = FLOAT_B(color);
+				data[i].color.a = FLOAT_A(color);
+			}
+
+			D3D11_BUFFER_DESC bufferDescLine{};
 			bufferDescLine.BindFlags = D3D11_BIND_INDEX_BUFFER;
-			bufferDescLine.ByteWidth = sizeof(Vertex) * 2;
+			bufferDescLine.ByteWidth = count * sizeof(Vertex);
 			bufferDescLine.Usage = D3D11_USAGE_DEFAULT;
 
-			Vertex pVerts[2]{
-				{ DirectX::XMFLOAT3(0, 0, 1.0f), { 0.0f, 1.0f, 0.0f, 1.0f } },
-				{ DirectX::XMFLOAT3(400, 400, 1.0f), { 0.0f, 1.0f, 0.0f, 1.0f } },
-			};
-
 			D3D11_SUBRESOURCE_DATA subresourceData{};
-			subresourceData.pSysMem = &pVerts;
+			subresourceData.pSysMem = data;
 
 			ID3D11Buffer* pVertexBuffer = nullptr;
 
 			this->_pDevice->CreateBuffer(&bufferDescLine, &subresourceData, &pVertexBuffer);
-			this->_pContext->VSSetConstantBuffers(0, 1, &this->_pConstantBuffer);
 
 			UINT stride = sizeof(Vertex);
 			UINT offset = 0;
-
 			this->_pContext->IASetVertexBuffers(0, 1, &pVertexBuffer, &stride, &offset);
-			this->_pContext->IASetInputLayout(this->_pVertexLayout);
+			this->_pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 
-			D3D11_PRIMITIVE_TOPOLOGY oldTopology;
-			this->_pContext->IAGetPrimitiveTopology(&oldTopology);
-			this->_pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
-
-			this->_pContext->VSSetShader(this->_pVertexShader, nullptr, 0);
-			this->_pContext->PSSetShader(this->_pPixelShader, nullptr, 0);
-
-			this->_pContext->Draw(2, 0);
+			this->_pContext->Draw(count, 0);
 
 			pVertexBuffer->Release();
-
-			this->_pContext->IASetPrimitiveTopology(oldTopology);
+			delete[] data;
 
 			return;
 		}
 
 
 		void Draw::drawString(void* pFont, const Vector2* pos, const char* text, rgb::Color color) const {
+			UNREFERENCED_PARAMETER(pFont);
+			UNREFERENCED_PARAMETER(pos);
+			UNREFERENCED_PARAMETER(text);
+			UNREFERENCED_PARAMETER(color);
 
 			return;
-		}
-
-
-		ID3D11RenderTargetView* Draw::createRenderTargetView() {
-			ID3D11Texture2D* pBackBuffer = nullptr;
-			HRESULT hResult = this->_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&pBackBuffer));
-				
-			if (hResult != S_OK || !pBackBuffer) return nullptr;
-
-			ID3D11RenderTargetView* pRenderTargetView = nullptr;
-
-			hResult = this->_pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &pRenderTargetView);
-			pBackBuffer->Release();
-				
-			if (hResult != S_OK || !pRenderTargetView) return nullptr;
-
-			return pRenderTargetView;
 		}
 
 
@@ -164,7 +229,7 @@ namespace hax {
 			PSI VS( float4 pos : POSITION, float4 color : COLOR )
 			{
 				PSI psi;
-				psi.pos = mul( projection, pos  );
+				psi.pos = mul( projection, pos );
 				psi.color = color;
 				return psi;
 			}
@@ -178,25 +243,28 @@ namespace hax {
 		bool Draw::compileShaders() {
 			ID3D10Blob* pCompiledVertexShader = nullptr;
 			HRESULT hResult = D3DCompile(shader, strlen(shader), 0, nullptr, nullptr, "VS", "vs_5_0", D3DCOMPILE_ENABLE_STRICTNESS, 0, &pCompiledVertexShader, nullptr);
-			
+
 			if (hResult != S_OK || !pCompiledVertexShader) return false;
 
 			hResult = this->_pDevice->CreateVertexShader(pCompiledVertexShader->GetBufferPointer(), pCompiledVertexShader->GetBufferSize(), nullptr, &this->_pVertexShader);
-			
-			if (hResult != S_OK || !this->_pVertexShader) return false;
 
-			D3D11_INPUT_ELEMENT_DESC inputElementDesc[2] {
+
+			if (hResult != S_OK || !this->_pVertexShader) {
+				pCompiledVertexShader->Release();
+
+				return false;
+			}
+
+			D3D11_INPUT_ELEMENT_DESC inputElementDesc[2]{
 				{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
 				{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0}
 			};
 
 			hResult = this->_pDevice->CreateInputLayout(inputElementDesc, sizeof(inputElementDesc) / sizeof(D3D11_INPUT_ELEMENT_DESC), pCompiledVertexShader->GetBufferPointer(), pCompiledVertexShader->GetBufferSize(), &this->_pVertexLayout);
 
-			if (hResult != S_OK) return false;
-
-			this->_pContext->IASetInputLayout(this->_pVertexLayout);
-			
 			pCompiledVertexShader->Release();
+
+			if (hResult != S_OK) return false;
 
 			ID3D10Blob* pCompiledPixelShader = nullptr;
 			hResult = D3DCompile(shader, strlen(shader), 0, nullptr, nullptr, "PS", "ps_5_0", D3DCOMPILE_ENABLE_STRICTNESS, 0, &pCompiledPixelShader, nullptr);
@@ -205,76 +273,28 @@ namespace hax {
 
 			hResult = this->_pDevice->CreatePixelShader(pCompiledPixelShader->GetBufferPointer(), pCompiledPixelShader->GetBufferSize(), nullptr, &this->_pPixelShader);
 
+			pCompiledPixelShader->Release();
+
 			if (hResult != S_OK || !this->_pPixelShader) return false;
 
 			return true;
 		}
 
-		
-		BOOL CALLBACK getMainWindowCallback(HWND hWnd, LPARAM lParam);
 
-		bool Draw::getViewport()
-		{
-			D3D11_VIEWPORT viewports[D3D11_VIEWPORT_AND_SCISSORRECT_OBJECT_COUNT_PER_PIPELINE]{};
-			UINT viewportCount = sizeof(viewports) / sizeof(D3D11_VIEWPORT);
+		bool Draw::setupOrtho(float screenWidth, float screenHeight) {
+			DirectX::XMMATRIX ortho = DirectX::XMMatrixOrthographicOffCenterLH(0, screenWidth, screenHeight, 0, 0.0f, 1.0f);
 
-			this->_pContext->RSGetViewports(&viewportCount, viewports);
-
-			if (viewportCount && viewports[0].Width) {
-				this->_viewport = viewports[0];
-			}
-			else {
-				HWND hMainWnd = nullptr;
-
-				EnumWindows(getMainWindowCallback, reinterpret_cast<LPARAM>(&hMainWnd));
-
-				if (!hMainWnd) return false;
-
-				RECT windowRect{};
-
-				if (!GetClientRect(hMainWnd, &windowRect)) return false;
-
-				this->_viewport.Width = static_cast<FLOAT>(windowRect.right);
-				this->_viewport.Height = static_cast<FLOAT>(windowRect.bottom);
-				this->_viewport.TopLeftX = static_cast<FLOAT>(windowRect.left);
-				this->_viewport.TopLeftY = static_cast<FLOAT>(windowRect.top);
-				this->_viewport.MinDepth = 0.0f;
-				this->_viewport.MaxDepth = 1.0f;
-				this->_pContext->RSSetViewports(1, &this->_viewport);
-			}
-
-			return true;
-		}
-
-
-		BOOL CALLBACK getMainWindowCallback(HWND hWnd, LPARAM lParam) {
-			DWORD processId = 0;
-			GetWindowThreadProcessId(hWnd, &processId);
-
-			if (!processId || GetCurrentProcessId() != processId || GetWindow(hWnd, GW_OWNER) || !IsWindowVisible(hWnd)) return TRUE;
-			
-			*reinterpret_cast<HWND*>(lParam) = hWnd;
-
-			return FALSE;
-		}
-
-
-		bool Draw::setupOrtho() {
 			D3D11_BUFFER_DESC bufferDesc{};
 			bufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-			bufferDesc.ByteWidth = sizeof(DirectX::XMMATRIX);
+			bufferDesc.ByteWidth = sizeof(ortho);
 			bufferDesc.Usage = D3D11_USAGE_DEFAULT;
 
 			D3D11_SUBRESOURCE_DATA subresourceData{};
-			DirectX::XMMATRIX ortho = DirectX::XMMatrixOrthographicOffCenterLH(0, this->_viewport.Width, this->_viewport.Height, 0, 0.0f, 1.0f);
 			subresourceData.pSysMem = &ortho;
-
 			const HRESULT hResult = this->_pDevice->CreateBuffer(&bufferDesc, &subresourceData, &this->_pConstantBuffer);
-			
+
 			if (hResult != S_OK || !this->_pConstantBuffer) return false;
 
-			this->_pContext->VSSetConstantBuffers(0, 1, &this->_pConstantBuffer);
-			
 			return true;
 		}
 
