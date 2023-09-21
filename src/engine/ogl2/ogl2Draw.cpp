1d816af39ce56bd29aeb0c51d00ab3cebbf6b33e
belazr <72708147+belazr@users.noreply.github.com>
2023-09-22 01:13:19 +0200

    Improved ogl2 performance by using vertex buffer

--- src/engine/ogl2/ogl2Draw.cpp
+++ src/engine/ogl2/ogl2Draw.cpp
@@ -9,17 +9,20 @@ namespace hax {
 
 		Draw::Draw() : _hGameContext{}, _hHookContext{}, _width{}, _height{},
 			_pglGenBuffers{}, _pglBindBuffer{}, _pglBufferData{}, _pglMapBuffer{},
-			_pglUnmapBuffer{}, _pglDeleteBuffers {}, _pglCreateShader{}, _pglShaderSource{}, _pglCompileShader{},
-			_triangleListBufferData{}, _isInit{} {}
+			_pglUnmapBuffer{}, _pglDeleteBuffers {}, _triangleListBufferData{}, _isInit{} {}
 
 
 		Draw::~Draw() {
 
 			if (this->_triangleListBufferData.indexBufferId) {
+				this->_pglBindBuffer(GL_ELEMENT_ARRAY_BUFFER, oldBufferData.indexBufferId);
+				this->_pglUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
 				this->_pglDeleteBuffers(1, &this->_triangleListBufferData.indexBufferId);
 			}
 			
 			if (this->_triangleListBufferData.vertexBufferId) {
+				this->_pglBindBuffer(GL_ARRAY_BUFFER, oldBufferData.vertexBufferId);
+				this->_pglUnmapBuffer(GL_ARRAY_BUFFER);
 				this->_pglDeleteBuffers(1, &this->_triangleListBufferData.vertexBufferId);
 			}
 
@@ -32,22 +35,14 @@ namespace hax {
 
 		constexpr GLenum GL_ARRAY_BUFFER = 0x8892;
 		constexpr GLenum GL_ELEMENT_ARRAY_BUFFER = 0x8893;
+		constexpr GLenum GL_WRITE_ONLY = 0x88B9;
 
 		void Draw::beginDraw(Engine* pEngine) {
 			const HDC hDc = reinterpret_cast<HDC>(pEngine->pHookArg);
 
 			if (!this->_isInit) {
-				this->_pglGenBuffers = reinterpret_cast<tglGenBuffers>(wglGetProcAddress("glGenBuffers"));
-				this->_pglBindBuffer = reinterpret_cast<tglBindBuffer>(wglGetProcAddress("glBindBuffer"));
-				this->_pglBufferData = reinterpret_cast<tglBufferData>(wglGetProcAddress("glBufferData"));
-				this->_pglMapBuffer = reinterpret_cast<tglMapBuffer>(wglGetProcAddress("glMapBuffer"));
-				this->_pglUnmapBuffer = reinterpret_cast<tglUnmapBuffer>(wglGetProcAddress("glUnmapBuffer"));
-				this->_pglDeleteBuffers = reinterpret_cast<tglDeleteBuffers>(wglGetProcAddress("glDeleteBuffers"));
-				this->_pglCreateShader = reinterpret_cast<tglCreateShader>(wglGetProcAddress("glCreateShader"));
-				this->_pglShaderSource = reinterpret_cast<tglShaderSource>(wglGetProcAddress("glShaderSource"));
-				this->_pglCompileShader = reinterpret_cast<tglCompileShader>(wglGetProcAddress("glCompileShader"));
 
-				if (!_pglGenBuffers || !_pglBindBuffer || !_pglBufferData || !_pglDeleteBuffers) return;
+				if (!this->getProcAddresses()) return;
 
 				this->_hGameContext = wglGetCurrentContext();
 				this->_hHookContext = wglCreateContext(hDc);
@@ -56,7 +51,7 @@ namespace hax {
 
 				if (!wglMakeCurrent(hDc, this->_hHookContext)) return;
 
-				constexpr GLsizei INITIAL_TRIANGLE_LIST_BUFFER_SIZE = sizeof(Vertex) * 100;
+				constexpr GLsizei INITIAL_TRIANGLE_LIST_BUFFER_SIZE = sizeof(Vertex) * 4;
 
 				if (!this->createBufferData(&this->_triangleListBufferData, INITIAL_TRIANGLE_LIST_BUFFER_SIZE)) return;
 				
@@ -64,8 +59,6 @@ namespace hax {
 			}
 
 			wglMakeCurrent(hDc, this->_hHookContext);
-
-			constexpr GLenum GL_WRITE_ONLY = 0x88B9;
 
 			this->_pglBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->_triangleListBufferData.indexBufferId);
 			this->_triangleListBufferData.pLocalIndexBuffer = reinterpret_cast<GLuint*>(this->_pglMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY));
@@ -100,10 +93,14 @@ namespace hax {
 			if (!this->_isInit) return;
 
 			glEnableClientState(GL_VERTEX_ARRAY);
+			glEnableClientState(GL_COLOR_ARRAY);
 
-			// vertex buffer currently bound
+			// vertex buffer is currently bound
 			this->_pglUnmapBuffer(GL_ARRAY_BUFFER);
-			glVertexPointer(2, GL_FLOAT, 0, NULL);
+
+			// third argument describes the offset in the Vertext struct, not the actuall address of the buffer
+			glVertexPointer(2, GL_FLOAT, sizeof(Vertex), nullptr);
+			glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(Vertex), reinterpret_cast<GLvoid*>(sizeof(Vector2)));
 
 			this->_pglBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->_triangleListBufferData.indexBufferId);
 			this->_pglUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
@@ -120,27 +117,18 @@ namespace hax {
 			
 			if (!this->_isInit || count % 3) return;
 			
-			//glColor3ub(UCHAR_R(color), UCHAR_G(color), UCHAR_B(color));
-			//glBegin(GL_TRIANGLES);
-
-			//for (UINT i = 0; i < count; i++) {
-			//	glVertex2f(corners[i].x, corners[i].y);
-			//}
-
-			//glEnd();
-
 			const UINT sizeNeeded = (this->_triangleListBufferData.curOffset + count) * sizeof(Vertex);
 
-			//if (sizeNeeded > pVertexBufferData->size) {
+			if (sizeNeeded > this->_triangleListBufferData.size) {
 
-			//	if (!this->resizeVertexBuffer(pVertexBufferData, sizeNeeded * 2)) return;
+				if (!this->resizeBuffers(&this->_triangleListBufferData, sizeNeeded * 2)) return;
 
-			//}
+			}
 
 			for (UINT i = 0; i < count; i++) {
 				const UINT curIndex = this->_triangleListBufferData.curOffset + i;
 
-				Vertex curVertex({ corners[i].x, corners[i].y });
+				Vertex curVertex{ { corners[i].x, corners[i].y }, color };
 				memcpy(&(this->_triangleListBufferData.pLocalVertexBuffer[curIndex]), &curVertex, sizeof(Vertex));
 
 				this->_triangleListBufferData.pLocalIndexBuffer[curIndex] = curIndex;
@@ -172,74 +160,27 @@ namespace hax {
 		}
 
 
-		const char* vertexShader{ R"(
-			uniform mat4 LProjectionMatrix;
-			uniform mat4 LModelViewMatrix;
+		bool Draw::getProcAddresses()
+		{
+			this->_pglGenBuffers = reinterpret_cast<tglGenBuffers>(wglGetProcAddress("glGenBuffers"));
+			this->_pglBindBuffer = reinterpret_cast<tglBindBuffer>(wglGetProcAddress("glBindBuffer"));
+			this->_pglBufferData = reinterpret_cast<tglBufferData>(wglGetProcAddress("glBufferData"));
+			this->_pglMapBuffer = reinterpret_cast<tglMapBuffer>(wglGetProcAddress("glMapBuffer"));
+			this->_pglUnmapBuffer = reinterpret_cast<tglUnmapBuffer>(wglGetProcAddress("glUnmapBuffer"));
+			this->_pglDeleteBuffers = reinterpret_cast<tglDeleteBuffers>(wglGetProcAddress("glDeleteBuffers"));
 
-			#if __VERSION__ >= 130
-
-			in vec2 LVertexPos2D;
-			in vec3 LMultiColor;
-
-			out vec4 multiColor;
-
-			#else
-
-			attribute vec2 LVertexPos2D;
-
-			attribute vec3 LMultiColor;
-			varying vec4 multiColor;
-
-			#endif
-
-			void main()
-			{
-				multiColor = vec4( LMultiColor.r, LMultiColor.g, LMultiColor.b, 1.0 );
-				gl_Position = LProjectionMatrix * LModelViewMatrix * vec4( LVertexPos2D.x, LVertexPos2D.y, 0.0, 1.0 );
+			if (
+				!this->_pglGenBuffers || !this->_pglBindBuffer || !this->_pglBufferData ||
+				!this->_pglMapBuffer || !this->_pglUnmapBuffer || !this->_pglDeleteBuffers
+			) {
+				return false;
 			}
-		)" };
-
-		const char* fragmentShader{ R"(
-			#if __VERSION__ >= 130
-
-			in vec4 multiColor;
-			out vec4 gl_FragColor;
-
-			#else
-
-			varying vec4 multiColor;
-
-			#endif
-
-			void main()
-			{
-				gl_FragColor = multiColor;
-			}
-		)" };
-
-		bool Draw::compileShaders() {
-			constexpr GLenum GL_CURRENT_PROGRAM = 0x8B8D;
-			GLint programId{};
-			glGetIntegerv(GL_CURRENT_PROGRAM, &programId);
-
-			constexpr GLenum GL_VERTEX_SHADER = 0x8B31;
-			const GLuint vertexShaderId = this->_pglCreateShader(GL_VERTEX_SHADER);
-			this->_pglShaderSource(vertexShaderId, 1, reinterpret_cast<const char**>(&vertexShader), NULL);
-			this->_pglCompileShader(vertexShaderId);
-
-			constexpr GLenum GL_FRAGMENT_SHADER = 0x8B30;
-			const GLuint fragmentShaderId = this->_pglCreateShader(GL_FRAGMENT_SHADER);
-			this->_pglShaderSource(fragmentShaderId, 1, reinterpret_cast<const char**>(&fragmentShader), NULL);
-			this->_pglCompileShader(fragmentShaderId);
-
-			//glAttachShader(mProgramID, fragmentShader);
-			//glLinkProgram(mProgramID);
 
 			return true;
 		}
 
 
-		bool Draw::createBufferData(BufferData* pBufferData, GLsizei size) const {
+		bool Draw::createBufferData(BufferData* pBufferData, UINT size) const {
 			constexpr GLenum GL_DYNAMIC_DRAW = 0x88E8;
 			
 			this->_pglGenBuffers(1, &pBufferData->indexBufferId);
@@ -258,6 +199,44 @@ namespace hax {
 			
 			pBufferData->size = size;
 
+			return true;
+		}
+
+
+		bool Draw::resizeBuffers(BufferData* pBufferData, UINT newSize) const {
+			const UINT bytesUsedVertex = pBufferData->curOffset * sizeof(Vertex);
+
+			if (newSize < bytesUsedVertex) return false;
+
+			const BufferData oldBufferData = *pBufferData;
+
+			if (!this->createBufferData(pBufferData, newSize)) return false;
+
+			this->_pglBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pBufferData->indexBufferId);
+			pBufferData->pLocalIndexBuffer = reinterpret_cast<GLuint*>(this->_pglMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY));
+
+			if (oldBufferData.pLocalIndexBuffer) {
+				memcpy(pBufferData->pLocalIndexBuffer, oldBufferData.pLocalIndexBuffer, bytesUsedVertex / sizeof(Vertex) * sizeof(GLuint));
+
+				this->_pglBindBuffer(GL_ELEMENT_ARRAY_BUFFER, oldBufferData.indexBufferId);
+				this->_pglUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+				this->_pglDeleteBuffers(1, &oldBufferData.indexBufferId);
+			}
+
+			this->_pglBindBuffer(GL_ARRAY_BUFFER, pBufferData->vertexBufferId);
+			pBufferData->pLocalVertexBuffer = reinterpret_cast<Vertex*>(this->_pglMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+
+			if (oldBufferData.pLocalVertexBuffer) {
+				memcpy(pBufferData->pLocalVertexBuffer, oldBufferData.pLocalVertexBuffer, bytesUsedVertex);
+
+				this->_pglBindBuffer(GL_ARRAY_BUFFER, oldBufferData.vertexBufferId);
+				this->_pglUnmapBuffer(GL_ARRAY_BUFFER);
+				this->_pglDeleteBuffers(1, &oldBufferData.vertexBufferId);
+			}
+
+			// bind the new vertex buffer as expected by endDraw()
+			this->_pglBindBuffer(GL_ARRAY_BUFFER, pBufferData->vertexBufferId);
+			
 			return true;
 		}
 
