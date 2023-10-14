ddbb888708a7a54e4d600933c26d1a3183e252ce
belazr <72708147+belazr@users.noreply.github.com>
2023-10-14 19:36:06 +0200

    Added graphics library linking to header file

--- src/hax.h
+++ src/hax.h
@@ -18,13 +18,20 @@
 #include "engine\dx\font\dxFonts.h"
 
 // Headers for DirectX 9
+#pragma comment(lib, "d3d9.lib")
+
 #include "engine\dx\dx9\dx9Draw.h"
 #include "engine\dx\dx9\dx9Vertex.h"
 
 // Headers for DirectX 11
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
 #include "engine\dx\dx11\dx11Draw.h"
 #include "engine\dx\dx11\dx11Vertex.h"
 
 // Headers for OpenGL 2
+#pragma comment( lib, "OpenGL32.lib" )
+
 #include "engine\ogl2\ogl2Draw.h"
 #include "engine\ogl2\ogl2Font.h"
