9835cc04a6e225ce52ba23dd2b7f3e2b2cd8040b
belazr <72708147+belazr@users.noreply.github.com>
2023-10-22 12:53:32 +0200

    Used untransformed vertecies in dx9::Draw

--- src/engine/dx/dx9/dx9Vertex.h
+++ src/engine/dx/dx9/dx9Vertex.h
@@ -11,14 +11,13 @@ namespace hax {
 		struct Vertex {
 		private:
 			Vector3 _coordinates;
-			float _rhw;
 			rgb::Color _color;
 
 			Vertex() = delete;
 
 		public:
 			Vertex(Vector2 coordinates, hax::rgb::Color color) :
-				_coordinates{ coordinates.x, coordinates.y, 1.f }, _rhw{ 1.f }, _color{ static_cast<rgb::Color>(UCHAR_A(color) << 24 | UCHAR_R(color) << 16 | UCHAR_G(color) << 8 | UCHAR_B(color)) } {}
+				_coordinates{ coordinates.x, coordinates.y, 1.f }, _color{ static_cast<rgb::Color>(UCHAR_A(color) << 24 | UCHAR_R(color) << 16 | UCHAR_G(color) << 8 | UCHAR_B(color)) } {}
 		};
 
 	}
