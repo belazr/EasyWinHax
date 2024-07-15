cf94e8b6a69815bd5e17d83e8ae16eb2e718747e
belazr <72708147+belazr@users.noreply.github.com>
2024-07-15 18:32:06 +0200

    Implemented dx11::DrawBuffer

---
+++ src/draw/dx/dx11/dx11DrawBuffer.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "..\..\AbstractDrawBuffer.h"
+#include <d3d11.h>
+
+namespace hax {
+
+	namespace draw {
+
+		namespace dx11 {
+
+			class DrawBuffer : public AbstractDrawBuffer {
+			private:
+				ID3D11Device* _pDevice;
+				ID3D11DeviceContext* _pContext;
+				ID3D11Buffer* _pVertexBuffer;
+				ID3D11Buffer* _pIndexBuffer;
+				D3D11_PRIMITIVE_TOPOLOGY _topology;
+
+			public:
+				DrawBuffer(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, D3D11_PRIMITIVE_TOPOLOGY topology);
+				~DrawBuffer();
+
+				bool create(uint32_t vertexCount) override;
+				void destroy() override;
+				bool map() override;
+				void draw() override;
+			};
+
+		}
+
+	}
+
+}
