efda5d2309225ce7496cbb18a2317e7342bc38f5
belazr <72708147+belazr@users.noreply.github.com>
2024-07-16 17:31:28 +0200

    Implemented vk::DrawBuffer

--- src/draw/vulkan/vkBackend.h
+++ src/draw/vulkan/vkBackend.h
@@ -1,10 +1,7 @@
 #pragma once
+#include "vkDefs.h"
 #include "..\Vertex.h"
 #include "..\IBackend.h"
-
-#define VK_NO_PROTOTYPES
-#include "include\vulkan.h"
-
 #include <Windows.h>
 
 namespace hax {
@@ -22,56 +19,6 @@ namespace hax {
 
 			class Backend : public IBackend {
 			private:
-				typedef struct Functions {
-					PFN_vkGetPhysicalDeviceMemoryProperties pVkGetPhysicalDeviceMemoryProperties;
-					PFN_vkGetSwapchainImagesKHR pVkGetSwapchainImagesKHR;
-					PFN_vkCreateCommandPool pVkCreateCommandPool;
-					PFN_vkDestroyCommandPool pVkDestroyCommandPool;
-					PFN_vkCreateRenderPass pVkCreateRenderPass;
-					PFN_vkDestroyRenderPass pVkDestroyRenderPass;
-					PFN_vkCreateShaderModule pVkCreateShaderModule;
-					PFN_vkDestroyShaderModule pVkDestroyShaderModule;
-					PFN_vkCreatePipelineLayout pVkCreatePipelineLayout;
-					PFN_vkDestroyPipelineLayout pVkDestroyPipelineLayout;
-					PFN_vkCreateDescriptorSetLayout pVkCreateDescriptorSetLayout;
-					PFN_vkDestroyDescriptorSetLayout pVkDestroyDescriptorSetLayout;
-					PFN_vkCreateGraphicsPipelines pVkCreateGraphicsPipelines;
-					PFN_vkDestroyPipeline pVkDestroyPipeline;
-					PFN_vkCreateBuffer pVkCreateBuffer;
-					PFN_vkDestroyBuffer pVkDestroyBuffer;
-					PFN_vkGetBufferMemoryRequirements pVkGetBufferMemoryRequirements;
-					PFN_vkAllocateMemory pVkAllocateMemory;
-					PFN_vkBindBufferMemory pVkBindBufferMemory;
-					PFN_vkFreeMemory pVkFreeMemory;
-					PFN_vkAllocateCommandBuffers pVkAllocateCommandBuffers;
-					PFN_vkFreeCommandBuffers pVkFreeCommandBuffers;
-					PFN_vkCreateImageView pVkCreateImageView;
-					PFN_vkDestroyImageView pVkDestroyImageView;
-					PFN_vkCreateFramebuffer pVkCreateFramebuffer;
-					PFN_vkDestroyFramebuffer pVkDestroyFramebuffer;
-					PFN_vkCreateFence pVkCreateFence;
-					PFN_vkDestroyFence pVkDestroyFence;
-					PFN_vkWaitForFences pVkWaitForFences;
-					PFN_vkResetFences pVkResetFences;
-					PFN_vkResetCommandBuffer pVkResetCommandBuffer;
-					PFN_vkBeginCommandBuffer pVkBeginCommandBuffer;
-					PFN_vkEndCommandBuffer pVkEndCommandBuffer;
-					PFN_vkCmdBeginRenderPass pVkCmdBeginRenderPass;
-					PFN_vkCmdEndRenderPass pVkCmdEndRenderPass;
-					PFN_vkMapMemory pVkMapMemory;
-					PFN_vkUnmapMemory pVkUnmapMemory;
-					PFN_vkFlushMappedMemoryRanges pVkFlushMappedMemoryRanges;
-					PFN_vkCmdBindPipeline pVkCmdBindPipeline;
-					PFN_vkCmdBindVertexBuffers pVkCmdBindVertexBuffers;
-					PFN_vkCmdBindIndexBuffer pVkCmdBindIndexBuffer;
-					PFN_vkCmdPushConstants pVkCmdPushConstants;
-					PFN_vkCmdSetViewport pVkCmdSetViewport;
-					PFN_vkCmdDrawIndexed pVkCmdDrawIndexed;
-					PFN_vkCmdSetScissor pVkCmdSetScissor;
-					PFN_vkGetDeviceQueue pVkGetDeviceQueue;
-					PFN_vkQueueSubmit pVkQueueSubmit;
-				}Functions;
-
 				typedef struct BufferData {
 					VkBuffer hVertexBuffer;
 					VkBuffer hIndexBuffer;
