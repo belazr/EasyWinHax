8d83627cb65646cd8ffa29af6db7a14e345a5678
belazr <72708147+belazr@users.noreply.github.com>
2024-07-17 10:16:33 +0200

    Implemented draw buffer logic in ogl2::Backend

--- src/draw/ogl2/ogl2Backend.cpp
+++ src/draw/ogl2/ogl2Backend.cpp
@@ -8,12 +8,12 @@ namespace hax {
 
 		namespace ogl2 {
 
-			Backend::Backend() : _f{}, _viewport{}, _triangleListBufferData{ UINT_MAX, UINT_MAX }, _pointListBufferData{ UINT_MAX, UINT_MAX } {}
+			Backend::Backend() : _f{}, _viewport{}, _depthFunc{}, _triangleListBuffer {}, _pointListBuffer{} {}
 
 
 			Backend::~Backend() {
-				this->destroyBufferData(&this->_pointListBufferData);
-				this->destroyBufferData(&this->_triangleListBufferData);
+				this->_pointListBuffer.destroy();
+				this->_triangleListBuffer.destroy();
 			}
 
 
@@ -30,29 +30,29 @@ namespace hax {
 
 				if (!this->getProcAddresses()) return false;
 
-				this->destroyBufferData(&this->_triangleListBufferData);
+				this->_triangleListBuffer.initialize(this->_f, GL_TRIANGLES);
+				this->_triangleListBuffer.destroy();
 
 				constexpr uint32_t INITIAL_TRIANGLE_LIST_BUFFER_SIZE = 99u;
 
-				if (!this->createBufferData(&this->_triangleListBufferData, INITIAL_TRIANGLE_LIST_BUFFER_SIZE)) return false;
+				if (!this->_triangleListBuffer.create(INITIAL_TRIANGLE_LIST_BUFFER_SIZE)) return false;
 
-				this->destroyBufferData(&this->_pointListBufferData);
+				this->_pointListBuffer.initialize(this->_f, GL_POINTS);
+				this->_pointListBuffer.destroy();
 
 				constexpr uint32_t INITIAL_POINT_LIST_BUFFER_SIZE = 1000u;
 
-				if (!this->createBufferData(&this->_pointListBufferData, INITIAL_POINT_LIST_BUFFER_SIZE)) return false;
+				if (!this->_pointListBuffer.create(INITIAL_POINT_LIST_BUFFER_SIZE)) return false;
 
 				return true;
 			}
 
 
 			bool Backend::beginFrame() {
-				
-				if (!this->mapBufferData(&this->_triangleListBufferData)) return false;
-
-				if (!this->mapBufferData(&this->_pointListBufferData)) return false;
-				
 				glGetIntegerv(GL_VIEWPORT, this->_viewport);
+				glGetIntegerv(GL_DEPTH_FUNC, reinterpret_cast<GLint*>(&this->_depthFunc));
+
+				glDepthFunc(GL_ALWAYS);
 
 				glMatrixMode(GL_PROJECTION);
 				glPushMatrix();
@@ -68,14 +68,13 @@ namespace hax {
 
 
 			void Backend::endFrame() {
-				this->drawBufferData(&this->_pointListBufferData, GL_POINTS);
-				this->drawBufferData(&this->_triangleListBufferData, GL_TRIANGLES);
-
 				glMatrixMode(GL_MODELVIEW);
 				glPopMatrix();
 
 				glMatrixMode(GL_PROJECTION);
 				glPopMatrix();
+
+				glDepthFunc(this->_depthFunc);
 
 				return;
 			}
@@ -86,6 +85,18 @@ namespace hax {
 				*frameHeight = static_cast<float>(this->_viewport[3]);
 
 				return;
+			}
+
+			
+			AbstractDrawBuffer* Backend::getTriangleListBuffer() {
+				
+				return &this->_triangleListBuffer;
+			}
+
+
+			AbstractDrawBuffer* Backend::getPointListBuffer() {
+
+				return &this->_pointListBuffer;
 			}
 
 
@@ -109,208 +120,6 @@ namespace hax {
 			}
 
 			#undef ASSIGN_PROC_ADDRESS
-
-
-			bool Backend::createBufferData(BufferData* pBufferData, uint32_t vertexCount) const {
-				RtlSecureZeroMemory(pBufferData, sizeof(BufferData));
-
-				const uint32_t vertexBufferSize = vertexCount * sizeof(Vertex);
-
-				if (!this->createBuffer(GL_ARRAY_BUFFER, GL_ARRAY_BUFFER_BINDING, vertexBufferSize, &pBufferData->vertexBufferId)) return false;
-
-				pBufferData->vertexBufferSize = vertexBufferSize;
-
-				const uint32_t indexBufferSize = vertexCount * sizeof(GLuint);
-
-				if (!this->createBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_ELEMENT_ARRAY_BUFFER_BINDING, indexBufferSize, &pBufferData->indexBufferId)) return false;
-
-				pBufferData->indexBufferSize = indexBufferSize;
-
-				return true;
-			}
-
-
-			void Backend::destroyBufferData(BufferData* pBufferData) const {
-
-				if (pBufferData->indexBufferId != UINT_MAX) {
-					GLuint curBufferId{};
-					glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, reinterpret_cast<GLint*>(&curBufferId));
-
-					this->_f.pGlBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pBufferData->indexBufferId);
-					this->_f.pGlUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
-					this->_f.pGlBindBuffer(GL_ELEMENT_ARRAY_BUFFER, curBufferId);
-
-					this->_f.pGlDeleteBuffers(1, &pBufferData->indexBufferId);
-					pBufferData->indexBufferId = UINT_MAX;
-				}
-
-				if (pBufferData->vertexBufferId != UINT_MAX) {
-					GLuint curArrayBufferId{};
-					glGetIntegerv(GL_ARRAY_BUFFER_BINDING, reinterpret_cast<GLint*>(&curArrayBufferId));
-
-					this->_f.pGlBindBuffer(GL_ARRAY_BUFFER, pBufferData->vertexBufferId);
-					this->_f.pGlUnmapBuffer(GL_ARRAY_BUFFER);
-					this->_f.pGlBindBuffer(GL_ARRAY_BUFFER, curArrayBufferId);
-
-					this->_f.pGlDeleteBuffers(1, &pBufferData->vertexBufferId);
-					pBufferData->vertexBufferId = UINT_MAX;
-				}
-
-
-				pBufferData->vertexBufferSize = 0ull;
-				pBufferData->pLocalVertexBuffer = nullptr;
-				pBufferData->indexBufferSize = 0ull;
-				pBufferData->pLocalIndexBuffer = nullptr;
-				pBufferData->curOffset = 0u;
-
-				return;
-			}
-
-
-			bool Backend::createBuffer(GLenum target, GLenum binding, uint32_t size, GLuint* pId) const {
-				this->_f.pGlGenBuffers(1, pId);
-
-				if (*pId == UINT_MAX) return false;
-
-				GLuint curBufferId = 0u;
-				glGetIntegerv(binding, reinterpret_cast<GLint*>(&curBufferId));
-
-				this->_f.pGlBindBuffer(target, *pId);
-				this->_f.pGlBufferData(target, size, nullptr, GL_DYNAMIC_DRAW);
-
-				this->_f.pGlBindBuffer(target, curBufferId);
-
-				return true;
-			}
-
-
-			bool Backend::mapBufferData(BufferData* pBufferData) const {
-
-				if (!pBufferData->pLocalVertexBuffer) {
-					GLuint curBufferId{};
-					glGetIntegerv(GL_ARRAY_BUFFER_BINDING, reinterpret_cast<GLint*>(&curBufferId));
-
-					this->_f.pGlBindBuffer(GL_ARRAY_BUFFER, pBufferData->vertexBufferId);
-
-					pBufferData->pLocalVertexBuffer = reinterpret_cast<Vertex*>(this->_f.pGlMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
-
-					this->_f.pGlBindBuffer(GL_ARRAY_BUFFER, curBufferId);
-
-					if (!pBufferData->pLocalVertexBuffer) return false;
-
-				}
-
-				if (!pBufferData->pLocalIndexBuffer) {
-					GLuint curBufferId{};
-					glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, reinterpret_cast<GLint*>(&curBufferId));
-
-					this->_f.pGlBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pBufferData->indexBufferId);
-
-					pBufferData->pLocalIndexBuffer = reinterpret_cast<uint32_t*>(this->_f.pGlMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY));
-
-					this->_f.pGlBindBuffer(GL_ELEMENT_ARRAY_BUFFER, curBufferId);
-
-					if (!pBufferData->pLocalIndexBuffer) return false;
-
-				}
-
-				return true;
-			}
-
-
-			void Backend::copyToBufferData(BufferData* pBufferData, const Vector2 data[], uint32_t count, rgb::Color color, Vector2 offset) const {
-				const uint32_t newVertexCount = pBufferData->curOffset + count;
-
-				if (newVertexCount * sizeof(Vertex) > pBufferData->vertexBufferSize || newVertexCount * sizeof(uint32_t) > pBufferData->indexBufferSize) {
-
-					if (!this->resizeBufferData(pBufferData, newVertexCount * 2u)) return;
-
-				}
-
-				if (!pBufferData->pLocalVertexBuffer || !pBufferData->pLocalIndexBuffer) return;
-
-				for (uint32_t i = 0u; i < count; i++) {
-					const uint32_t curIndex = pBufferData->curOffset + i;
-
-					const Vertex curVertex{ { data[i].x + offset.x, data[i].y + offset.y }, color };
-					memcpy(&(pBufferData->pLocalVertexBuffer[curIndex]), &curVertex, sizeof(Vertex));
-
-					pBufferData->pLocalIndexBuffer[curIndex] = curIndex;
-				}
-
-				pBufferData->curOffset += count;
-
-				return;
-			}
-
-
-			bool Backend::resizeBufferData(BufferData* pBufferData, uint32_t newVertexCount) const {
-
-				if (newVertexCount <= pBufferData->curOffset) return true;
-
-				BufferData oldBufferData = *pBufferData;
-
-				if (!this->createBufferData(pBufferData, newVertexCount)) {
-					this->destroyBufferData(pBufferData);
-
-					return false;
-				}
-
-				if (!this->mapBufferData(pBufferData)) return false;
-
-
-				if (oldBufferData.pLocalVertexBuffer) {
-					memcpy(pBufferData->pLocalVertexBuffer, oldBufferData.pLocalVertexBuffer, oldBufferData.vertexBufferSize);
-				}
-
-				if (oldBufferData.pLocalIndexBuffer) {
-					memcpy(pBufferData->pLocalIndexBuffer, oldBufferData.pLocalIndexBuffer, oldBufferData.indexBufferSize);
-				}
-
-				pBufferData->curOffset = oldBufferData.curOffset;
-
-				this->destroyBufferData(&oldBufferData);
-
-				return true;
-			}
-
-
-			void Backend::drawBufferData(BufferData* pBufferData, GLenum mode) const {
-				GLuint curVertexBufferId{};
-				glGetIntegerv(GL_ARRAY_BUFFER_BINDING, reinterpret_cast<GLint*>(&curVertexBufferId));
-
-				GLuint curIndexBufferId{};
-				glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, reinterpret_cast<GLint*>(&curIndexBufferId));
-
-				this->_f.pGlBindBuffer(GL_ARRAY_BUFFER, pBufferData->vertexBufferId);
-				this->_f.pGlBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pBufferData->indexBufferId);
-
-				glEnableClientState(GL_VERTEX_ARRAY);
-				glEnableClientState(GL_COLOR_ARRAY);
-
-				// third argument describes the offset in the Vertext struct, not the actuall address of the buffer
-				glVertexPointer(2, GL_FLOAT, sizeof(Vertex), nullptr);
-				glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(Vertex), reinterpret_cast<GLvoid*>(sizeof(Vector2)));
-
-				this->_f.pGlUnmapBuffer(GL_ARRAY_BUFFER);
-				pBufferData->pLocalVertexBuffer = nullptr;
-
-				this->_f.pGlUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
-				pBufferData->pLocalIndexBuffer = nullptr;
-
-				glDrawElements(mode, static_cast<GLsizei>(pBufferData->curOffset), GL_UNSIGNED_INT, nullptr);
-
-				glDisableClientState(GL_COLOR_ARRAY);
-				glDisableClientState(GL_VERTEX_ARRAY);
-
-				pBufferData->curOffset = 0u;
-
-				this->_f.pGlBindBuffer(GL_ARRAY_BUFFER, curVertexBufferId);
-				this->_f.pGlBindBuffer(GL_ELEMENT_ARRAY_BUFFER, curIndexBufferId);
-
-				return;
-			}
-
 		}
 
 	}
