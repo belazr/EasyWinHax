4f7c587c0311b4d5f057933be8dd10e9681aa700
belazr <72708147+belazr@users.noreply.github.com>
2024-08-01 18:58:33 +0200

    Implemented dx12::Backend::setHookArguments

--- src/draw/dx/dx12/dx12Backend.h
+++ src/draw/dx/dx12/dx12Backend.h
@@ -31,6 +31,62 @@ namespace hax {
 			bool getDXGISwapChain3VTable(void** pSwapChainVTable, size_t size);
 
 			class Backend : public IBackend {
+			private:
+				IDXGISwapChain3* _pSwapChain;
+
+			public:
+				Backend();
+
+				~Backend();
+
+				// Initializes backend and starts a frame within a hook. Should be called by an Engine object.
+				//
+				// Parameters:
+				// 
+				// [in] pArg1:
+				// Pass the IDXGISwapChain3*.
+				//
+				// [in] pArg2:
+				// Pass nothing
+				virtual void setHookArguments(void* pArg1 = nullptr, void* pArg2 = nullptr) override;
+
+				// Initializes the backend. Should be called by an Engine object until success.
+				// 
+				// Return:
+				// True on success, false on failure.
+				virtual bool initialize() override;
+
+				// Starts a frame within a hook. Should be called by an Engine object every frame at the begin of the hook.
+				// 
+				// Return:
+				// True on success, false on failure.
+				virtual bool beginFrame() override;
+
+				// Ends the current frame within a hook. Should be called by an Engine object every frame at the end of the hook.
+				virtual void endFrame() override;
+
+				// Gets a reference to the triangle list buffer of the backend. It is the responsibility of the backend to dispose of the buffer properly.
+				// 
+				// Return:
+				// Pointer to the triangle list buffer.
+				virtual AbstractDrawBuffer* getTriangleListBuffer() override;
+
+				// Gets a reference to the point list buffer of the backend. It is the responsibility of the backend to dispose of the buffer properly.
+				// 
+				// Return:
+				// Pointer to the point list buffer.
+				virtual AbstractDrawBuffer* getPointListBuffer() override;
+
+				// Gets the resolution of the current frame. Should be called by an Engine object.
+				//
+				// Parameters:
+				// 
+				// [out] frameWidth:
+				// Pointer that receives the current frame width in pixel.
+				//
+				// [out] frameHeight:
+				// Pointer that receives the current frame height in pixel.
+				virtual void getFrameResolution(float* frameWidth, float* frameHeight) override;
 			};
 
 		}
