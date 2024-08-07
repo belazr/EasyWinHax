26fb4227078e72b417ed614b722d990c6df84474
belazr <72708147+belazr@users.noreply.github.com>
2024-08-07 20:56:05 +0200

    Implemented dx12::DrawBuffer::create

--- src/draw/dx/dx12/dx12DrawBuffer.cpp
+++ src/draw/dx/dx12/dx12DrawBuffer.cpp
@@ -1,1 +1,65 @@
 #include "dx12DrawBuffer.h"
+
+namespace hax {
+
+	namespace draw {
+
+		namespace dx12 {
+
+			DrawBuffer::DrawBuffer() : _pDevice{}, _pVertexBufferResource {}, _pIndexBufferResource{} {}
+
+
+			DrawBuffer::~DrawBuffer() {
+
+				return;
+			}
+
+			void DrawBuffer::initialize(ID3D12Device* pDevice) {
+				this->_pDevice = pDevice;
+
+				return;
+			}
+
+
+			bool DrawBuffer::create(uint32_t vertexCount) {
+				this->reset();
+
+				this->_pVertexBufferResource = nullptr;
+				this->_pIndexBufferResource = nullptr;
+
+				const uint32_t vertexBufferSize = vertexCount * sizeof(Vertex);
+
+				if (!this->createBuffer(&this->_pVertexBufferResource, vertexBufferSize)) return false;
+
+				this->_vertexBufferSize = vertexBufferSize;
+
+				const uint32_t indexBufferSize = vertexCount * sizeof(uint32_t);
+
+				if (!this->createBuffer(&this->_pIndexBufferResource, indexBufferSize)) return false;
+
+				this->_indexBufferSize = indexBufferSize;
+
+				return true;
+			}
+
+
+			bool DrawBuffer::createBuffer(ID3D12Resource** ppBufferResource, uint32_t size) const {
+				D3D12_HEAP_PROPERTIES heapProperties{};
+				heapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
+				heapProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+				heapProperties.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+
+				D3D12_RESOURCE_DESC resourceDesc{};
+				resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+				resourceDesc.Width = size;
+				resourceDesc.Height = 1u;
+				resourceDesc.DepthOrArraySize = 1u;
+	
+				return SUCCEEDED(this->_pDevice->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &resourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(ppBufferResource)));
+			}
+
+		}
+
+	}
+
+}
--- src/draw/dx/dx12/dx12DrawBuffer.h
+++ src/draw/dx/dx12/dx12DrawBuffer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "..\..\AbstractDrawBuffer.h"
+#include <d3d12.h>
 
 namespace hax {
 
@@ -8,6 +9,37 @@ namespace hax {
 		namespace dx12 {
 
 			class DrawBuffer : public AbstractDrawBuffer {
+			private:
+				ID3D12Device* _pDevice;
+				ID3D12Resource* _pVertexBufferResource;
+				ID3D12Resource* _pIndexBufferResource;
+
+			public:
+				DrawBuffer();
+				~DrawBuffer();
+
+				// Initializes members.
+				//
+				// Parameters:
+				// 
+				// [in] hDevice:
+				// Logical device of the backend.
+				void initialize(ID3D12Device* pDevice);
+
+				// Creates a new buffer with all internal resources.
+				//
+				// Parameters:
+				// 
+				// [in] vertexCount:
+				// Size of the buffer in vertices.
+				//
+				// Return:
+				// True on success, false on failure.
+				bool create(uint32_t vertexCount) override;
+
+			private:
+				bool createBuffer(ID3D12Resource** ppBufferResource, uint32_t size) const;
+
 			};
 
 		}
