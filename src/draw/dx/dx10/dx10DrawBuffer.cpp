138d6524bbad66e53cc57188c0e40c86d4871e98
belazr <72708147+belazr@users.noreply.github.com>
2025-01-21 00:29:12 +0100

    Implemented dx10::DrawBuffer::initialize

--- src/draw/dx/dx10/dx10DrawBuffer.cpp
+++ src/draw/dx/dx10/dx10DrawBuffer.cpp
@@ -1,1 +1,23 @@
 #include "dx10DrawBuffer.h"
+
+namespace hax {
+
+	namespace draw {
+
+		namespace dx10 {
+
+			DrawBuffer::DrawBuffer() : AbstractDrawBuffer(), _pDevice{}, _topology{} {}
+
+
+			void DrawBuffer::initialize(ID3D10Device* pDevice, D3D10_PRIMITIVE_TOPOLOGY topology) {
+				this->_pDevice = pDevice;
+				this->_topology = topology;
+
+				return;
+			}
+
+		}
+
+	}
+
+}
