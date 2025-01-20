256441e88d248550fadf1bdd57f160600be7ab2a
belazr <72708147+belazr@users.noreply.github.com>
2025-01-21 00:29:12 +0100

    Implemented dx10::DrawBuffer::initialize

--- src/draw/dx/dx10/dx10DrawBuffer.h
+++ src/draw/dx/dx10/dx10DrawBuffer.h
@@ -10,11 +10,27 @@ namespace hax {
 
 			class DrawBuffer : public AbstractDrawBuffer {
 			private:
+				ID3D10Device* _pDevice;
+				D3D10_PRIMITIVE_TOPOLOGY _topology;
 
 			public:
 				DrawBuffer();
 
 				~DrawBuffer();
+
+				// Initializes members.
+				//
+				// Parameters:
+				// 
+				// [in] pDevice:
+				// Device of the backend.
+				// 
+				// [in] pContext:
+				// Context of the backend.
+				// 
+				// [in] topology:
+				// Primitive topology the vertices in the buffer should be drawn in.
+				void initialize(ID3D10Device* pDevice, D3D10_PRIMITIVE_TOPOLOGY topology);
 			};
 
 		}
