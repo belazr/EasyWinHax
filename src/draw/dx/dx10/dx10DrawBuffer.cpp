a66cccbeea51a9ba67f5048cd5544eaa9abf37fa
belazr <72708147+belazr@users.noreply.github.com>
2025-01-23 21:49:47 +0100

    Implemented dx10::DrawBuffer::create

--- src/draw/dx/dx10/dx10DrawBuffer.cpp
+++ src/draw/dx/dx10/dx10DrawBuffer.cpp
@@ -6,7 +6,7 @@ namespace hax {
 
 		namespace dx10 {
 
-			DrawBuffer::DrawBuffer() : AbstractDrawBuffer(), _pDevice{}, _topology{} {}
+			DrawBuffer::DrawBuffer() : AbstractDrawBuffer(), _pDevice{}, _topology{}, _pVertexBuffer{}, _pIndexBuffer{} {}
 
 
 			void DrawBuffer::initialize(ID3D10Device* pDevice, D3D10_PRIMITIVE_TOPOLOGY topology) {
@@ -14,6 +14,36 @@ namespace hax {
 				this->_topology = topology;
 
 				return;
+			}
+
+
+			bool DrawBuffer::create(uint32_t vertexCount) {
+				this->reset();
+
+				this->_pVertexBuffer = nullptr;
+				this->_pIndexBuffer = nullptr;
+
+				D3D10_BUFFER_DESC vertexBufferDesc{};
+				vertexBufferDesc.BindFlags = D3D10_BIND_VERTEX_BUFFER;
+				vertexBufferDesc.ByteWidth = vertexCount * sizeof(Vertex);
+				vertexBufferDesc.Usage = D3D10_USAGE_DYNAMIC;
+				vertexBufferDesc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
+
+				if (FAILED(this->_pDevice->CreateBuffer(&vertexBufferDesc, nullptr, &this->_pVertexBuffer))) return false;
+
+				this->_vertexBufferSize = vertexBufferDesc.ByteWidth;
+
+				D3D10_BUFFER_DESC indexBufferDesc{};
+				indexBufferDesc.BindFlags = D3D10_BIND_INDEX_BUFFER;
+				indexBufferDesc.ByteWidth = vertexCount * sizeof(uint32_t);
+				indexBufferDesc.Usage = D3D10_USAGE_DYNAMIC;
+				indexBufferDesc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
+
+				if (FAILED(this->_pDevice->CreateBuffer(&indexBufferDesc, nullptr, &this->_pIndexBuffer))) return false;
+
+				this->_indexBufferSize = indexBufferDesc.ByteWidth;
+
+				return true;
 			}
 
 		}
--- src/draw/dx/dx10/dx10DrawBuffer.h
+++ src/draw/dx/dx10/dx10DrawBuffer.h
@@ -12,6 +12,8 @@ namespace hax {
 			private:
 				ID3D10Device* _pDevice;
 				D3D10_PRIMITIVE_TOPOLOGY _topology;
+				ID3D10Buffer* _pVertexBuffer;
+				ID3D10Buffer* _pIndexBuffer;
 
 			public:
 				DrawBuffer();
@@ -31,6 +33,17 @@ namespace hax {
 				// [in] topology:
 				// Primitive topology the vertices in the buffer should be drawn in.
 				void initialize(ID3D10Device* pDevice, D3D10_PRIMITIVE_TOPOLOGY topology);
+
+				// Creates a new buffer with all internal resources.
+				//
+				// Parameters:
+				// 
+				// [in] vertexCount:
+				// Size of the buffer in vertices.
+				//
+				// Return:
+				// True on success, false on failure.
+				bool create(uint32_t vertexCount) override;
 			};
 
 		}
