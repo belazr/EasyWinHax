046eb3ee2450f5da0a766c5f88a27d0505928fd7
belazr <72708147+belazr@users.noreply.github.com>
2025-03-17 18:16:36 +0100

    Added hax::Vector implementation

---
+++ src/Vector.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+
+// Basic vector class inspired by the STL version. It does only what is needed in this library and ommits safety checks for performance reasons.
+// It never calls destructors of the contained elements!
+
+namespace hax {
+
+	template<typename T>
+	class Vector {
+	private:
+		T* _data;
+		size_t _size;
+		size_t _capacity;
+
+	public:
+		Vector() = delete;
+
+		Vector(const Vector&) = delete;
+		Vector& operator=(const Vector&) = delete;
+
+		Vector(Vector&&) = delete;
+		Vector& operator=(Vector&&) = delete;
+
+
+		// Initializes a vector with an initial capacity.
+		// 
+		// Parameters:
+		// 
+		// [in] capacity:
+		// The initial capacity.
+		Vector(size_t capacity) : _data{}, _size{}, _capacity{} {
+			this->reserve(capacity);
+		}
+
+
+		~Vector() {
+			free(this->_data);
+		}
+
+
+		T* operator[](size_t i) const {
+			
+			return &data[i];
+		}
+
+
+		// Increases the capacity of the vector if the desired capacity is greater than the current one.
+		// 
+		// Parameters:
+		// 
+		// [in] capacity:
+		// The desired capacity.
+		void reserve(size_t capacity) {
+
+			if (capacity <= this->_capacity) return;
+
+			T* data = reinterpret_cast<T*>(calloc(capacity, sizeof(T)));
+
+			if (this->_data && this->_size) {
+				memcpy(data, this->_data, this->_size * sizeof(T));
+				free(this->_data);
+			}
+			
+			this->_data = data;
+			this->_capacity = capacity;
+		}
+
+
+		// Appends an element to the vector.
+		// 
+		// Parameters:
+		// 
+		// [in] t:
+		// The element to append.
+		void append(T t) {
+
+			if (this->_size == this->_capacity) {
+				this->reserve(this->_capacity + this->_capacity / 2u);
+			}
+
+			this->_data[this->_size] = t;
+			this->_size++;
+		}
+	};
+}
