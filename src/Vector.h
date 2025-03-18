300e9b3d5256e78740f1d62dda800d787aec80bb
belazr <benediktlazar92@gmail.com>
2025-03-18 16:54:04 +0100

    Implemented hax::Vector::resize

--- src/Vector.h
+++ src/Vector.h
@@ -95,6 +95,40 @@ namespace hax {
 		}
 
 
+		// Resizes the vector.
+		// If the desired size is smaller than the current one, calls the destructors of the excessive elements.
+		// If the desired size is greater than the current one, calls the trival constructors of the new elements.
+		// 
+		// Parameters:
+		// 
+		// [in] capacity:
+		// The desired capacity.
+		void resize(size_t size) {
+
+			if (size == this->_size) return;
+
+			if (size > this->_size) {
+				this->reserve(2u * size);
+
+				for (size_t i = this->_size; i < size; i++) {
+					this->_data[i] = T();
+				}
+
+			}
+			else {
+
+				for (size_t i = this->_size - 1u; i >= size; i--) {
+					this->_data[i].~T();
+				}
+
+			}
+
+			this->_size = size;
+
+			return;
+		}
+
+
 		// Appends an element to the vector.
 		// 
 		// Parameters:
@@ -104,7 +138,7 @@ namespace hax {
 		void append(const T& t) {
 
 			if (this->_size == this->_capacity) {
-				const size_t capacity = this->_capacity ? this->_capacity + this->_capacity / 2u : 8u;
+				const size_t capacity = this->_capacity ? 2 * this->_capacity : 8u;
 				this->reserve(capacity);
 			}
 
