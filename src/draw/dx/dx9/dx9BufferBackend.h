09d2fdc8f65a617e9943129d086253703506c78a
belazr <72708147+belazr@users.noreply.github.com>
2025-04-15 00:19:56 +0200

    Implemented dx9::BufferBackend

---
+++ src/draw/dx/dx9/dx9BufferBackend.h
@@ -0,0 +1,95 @@
+#pragma once
+#include "..\..\IBufferBackend.h"
+#include <d3d9.h>
+
+namespace hax {
+
+	namespace draw {
+
+		namespace dx9 {
+
+			class BufferBackend : public IBufferBackend {
+			private:
+				IDirect3DDevice9* _pDevice;
+				IDirect3DPixelShader9* _pPixelShader;
+				IDirect3DVertexBuffer9* _pVertexBuffer;
+				IDirect3DIndexBuffer9* _pIndexBuffer;
+				D3DPRIMITIVETYPE _primitiveType;
+
+				uint32_t _capacity;
+
+			public:
+				BufferBackend();
+
+				~BufferBackend();
+
+				// Initializes members.
+				//
+				// Parameters:
+				// 
+				// [in] pDevice:
+				// Device of the backend.
+				//
+				// [in] pPixelShader:
+				// Pixel shader for drawing without textures.
+				//
+				// [in] primitiveType:
+				// Primitive topology the vertices in the buffer should be drawn in.
+				void initialize(IDirect3DDevice9* pDevice, IDirect3DPixelShader9* pPixelShader, D3DPRIMITIVETYPE primitiveType);
+
+				// Creates internal resources.
+				//
+				// Parameters:
+				// 
+				// [in] capacity:
+				// Capacity of verticies the buffer backend can hold.
+				//
+				// Return:
+				// True on success, false on failure.
+				bool create(uint32_t capacity) override;
+
+				// Destroys all internal resources.
+				void destroy() override;
+
+				// Maps the allocated VRAM into the address space of the current process.
+				// 
+				// Parameters:
+				// 
+				// [out] ppLocalVertexBuffer:
+				// The mapped vertex buffer.
+				//
+				// 
+				// [out] ppLocalIndexBuffer:
+				// The mapped index buffer.
+				//
+				// Return:
+				// True on success, false on failure.
+				bool map(Vertex** ppLocalVertexBuffer, uint32_t** ppLocalIndexBuffer) override;
+
+				// Prepares the buffer backend for drawing a batch.
+				//
+				// Return:
+				// True on success, false on failure.
+				bool prepare() override;
+
+				// Draws a batch.
+				// 
+				// Parameters:
+				// 
+				// [in] textureId:
+				// ID of the texture that should be drawn return by Backend::loadTexture.
+				// If this is 0ull, no texture will be drawn.
+				//
+				// [in] index:
+				// Index into the index buffer where the batch begins.
+				// 
+				// [in] count:
+				// Vertex count in the batch.
+				void draw(TextureId textureId, uint32_t index, uint32_t count) override;
+			};
+
+		}
+
+	}
+
+}
