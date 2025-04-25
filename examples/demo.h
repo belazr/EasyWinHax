3b8b97a4e22aaf9d9c3d770e095f58e9778fb615
belazr <72708147+belazr@users.noreply.github.com>
2025-04-25 22:33:22 +0200

    Added texture to demo

--- examples/demo.h
+++ examples/demo.h
@@ -1,24 +1,26 @@
 #pragma once
-#include "..\src\hax.h"
+#include "demoTexture.h"
 
 inline void drawDemo(hax::draw::Engine* pEngine, bool argb = false) {
+	static hax::draw::TextureId textureId = pEngine->loadTexture(DEMO_TEXTURE, DEMO_TEXTURE_WIDTH, DEMO_TEXTURE_HEIGHT);
+	
 	const hax::Vector2 middleOfScreen{ pEngine->frameWidth / 2.f, pEngine->frameHeight / 2.f };
 
-	const float widthRect = pEngine->frameWidth / 4.f;
-	const float heightRect = pEngine->frameHeight / 4.f;
-	const hax::Vector2 topLeftRect{ middleOfScreen.x - widthRect / 2.f, middleOfScreen.y - heightRect / 2.f };
-	const hax::draw::Color rectColor = argb ? hax::draw::argb::GRAY : hax::draw::abgr::GRAY;
+	if (textureId) {
+		const float widthTexture = pEngine->frameWidth / 2.f;
+		const float heightTexture = widthTexture * static_cast<float>(DEMO_TEXTURE_HEIGHT) / static_cast<float>(DEMO_TEXTURE_WIDTH);
+		const hax::Vector2 topLeftTexture{ middleOfScreen.x - widthTexture / 2.f, middleOfScreen.y - heightTexture / 2.f };
 
-	pEngine->drawFilledRectangle(&topLeftRect, widthRect, heightRect, rectColor);
+		pEngine->drawTexture(textureId, &topLeftTexture, widthTexture, heightTexture);
+	}
 
 	constexpr char TEXT[] = "EasyWinHax";
 	const float widthText = _countof(TEXT) * hax::draw::font::medium.width;
 	const float heightText = hax::draw::font::medium.height;
-
 	const hax::Vector2 bottomLeftText{ middleOfScreen.x - widthText / 2.f, middleOfScreen.y + heightText / 2.f };
-	const hax::draw::Color textColor = argb ? hax::draw::argb::ORANGE : hax::draw::abgr::ORANGE;
+	const hax::draw::Color colorText = argb ? hax::draw::argb::ORANGE : hax::draw::abgr::ORANGE;
 
-	pEngine->drawString(&hax::draw::font::medium, &bottomLeftText, TEXT, textColor);
+	pEngine->drawString(&hax::draw::font::medium, &bottomLeftText, TEXT, colorText);
 
 	return;
 }
