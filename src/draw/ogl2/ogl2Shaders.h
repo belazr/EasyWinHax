3af54bb90dce556f9f3cd9296390da3232f80bbf
belazr <72708147+belazr@users.noreply.github.com>
2025-05-08 00:02:23 +0200

    Added ogl2Shaders.h

---
+++ src/draw/ogl2/ogl2Shaders.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "ogl2Defs.h"
+
+namespace hax {
+
+	namespace draw {
+
+		namespace ogl2 {
+
+			constexpr const GLchar* VERTEX_SHADER =
+				"#version 120\n"
+
+				"uniform mat4 projectionMatrix;\n"
+
+				"attribute vec2 pos;\n"
+				"attribute vec4 col;\n"
+				"attribute vec2 uv;\n"
+
+				"varying vec4 colOut;\n"
+				"varying vec2 uvOut;\n"
+
+				"void main() {\n"
+				"    colOut = col;\n"
+				"    uvOut = uv;\n"
+				"    gl_Position = projectionMatrix * vec4(pos.xy,0,1);\n"
+				"}\n";
+
+			constexpr const GLchar* FRAGMENT_SHADER_PASSTHROUGH =
+				"#version 120\n"
+
+				"varying vec4 colOut;\n"
+
+				"void main() {\n"
+				"    gl_FragColor = colOut;\n"
+				"}\n";
+
+			constexpr const GLchar* FRAGMENT_SHADER_TEXTURE =
+				"#version 120\n"
+
+				"uniform sampler2D texSampler;\n"
+
+				"varying vec4 colOut;\n"
+				"varying vec2 uvOut;\n"
+
+				"void main() {\n"
+				"    gl_FragColor = colOut * texture2D(texSampler, uvOut);\n"
+				"}\n";
+		}
+
+	}
+
+}
