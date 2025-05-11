c7d7a9fc8ee7bad627674b57821835c8c3894e4e
belazr <benediktlazar92@gmail.com>
2025-05-12 00:44:50 +0200

    Set texture sampler filters to linear in ogl2::Backend

--- src/draw/ogl2/ogl2Backend.cpp
+++ src/draw/ogl2/ogl2Backend.cpp
@@ -51,8 +51,8 @@ namespace hax {
 				GLuint textureId = 0u;
 				glGenTextures(1, &textureId);
 				glBindTexture(GL_TEXTURE_2D, textureId);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 				glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 				
