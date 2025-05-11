9daad85a178902ebb34b63f03a50b8bbef11e650
belazr <benediktlazar92@gmail.com>
2025-05-12 00:38:39 +0200

    Set texture sampler filters to linear in dx11::Backend

--- src/draw/dx/dx11/dx11Backend.cpp
+++ src/draw/dx/dx11/dx11Backend.cpp
@@ -331,14 +331,11 @@ namespace hax {
 
 			bool Backend::createSamplerState() {
 				D3D11_SAMPLER_DESC samplerDesc{};
-				samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+				samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
 				samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 				samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 				samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-				samplerDesc.MipLODBias = 0.f;
 				samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
-				samplerDesc.MinLOD = 0.f;
-				samplerDesc.MaxLOD = 0.f;
 
 				return SUCCEEDED(this->_pDevice->CreateSamplerState(&samplerDesc, &this->_pSamplerState));
 			}
