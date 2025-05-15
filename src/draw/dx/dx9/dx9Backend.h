ee755af3fdcec029c74c0dfec1e4a969a9ab268c
belazr <benediktlazar92@gmail.com>
2025-05-16 01:01:36 +0200

    Removed dx9::Backend::getPointListBufferBackend declaration

--- src/draw/dx/dx9/dx9Backend.h
+++ src/draw/dx/dx9/dx9Backend.h
@@ -93,23 +93,17 @@ namespace hax {
 				// Ends the current frame within a hook. Should be called by an Engine object every frame at the end of the hook.
 				virtual void endFrame() override;
 
-				// Gets a reference to the point list buffer backend. It is the responsibility of the backend to dispose of the buffer backend properly.
+				// Gets a reference to the triangle list buffer backend. It is the responsibility of the backend to dispose of the buffer backend properly.
 				// 
 				// Return:
-				// Pointer to the point list buffer backend.
-				virtual IBufferBackend* getPointListBufferBackend()  override;
+				// Pointer to the triangle list buffer backend.
+				virtual IBufferBackend* getTriangleListBufferBackend()  override;
 
 				// Gets a reference to the texture triangle list buffer backend. It is the responsibility of the backend to dispose of the buffer backend properly.
 				// 
 				// Return:
 				// Pointer to the texture triangle list buffer backend.
 				virtual IBufferBackend* getTextureTriangleListBufferBackend()  override;
-
-				// Gets a reference to the triangle list buffer backend. It is the responsibility of the backend to dispose of the buffer backend properly.
-				// 
-				// Return:
-				// Pointer to the triangle list buffer backend.
-				virtual IBufferBackend* getTriangleListBufferBackend()  override;
 
 				// Gets the resolution of the current frame. Should be called by an Engine object.
 				//
