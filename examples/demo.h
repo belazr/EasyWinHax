b5ff4c0a3cc2b7110b28d2229cdc4f7c2952b38f
belazr <72708147+belazr@users.noreply.github.com>
2025-05-19 00:14:44 +0200

    Updated demo for drawing strings with font atlas

--- examples/demo.h
+++ examples/demo.h
@@ -9,18 +9,14 @@ inline void drawDemo(hax::draw::Engine* pEngine, const hax::draw::Color* pTextur
 	if (textureId) {
 		const float widthTextureRect = pEngine->frameWidth / 4.f;
 		const float heightTextureRect = widthTextureRect * static_cast<float>(textureHeight) / static_cast<float>(textureWidth);
-		const hax::Vector2 topLeftTexture{ middleOfScreen.x - widthTextureRect / 2.f, middleOfScreen.y - heightTextureRect / 2.f };
 
-		pEngine->drawTexture(textureId, &topLeftTexture, widthTextureRect, heightTextureRect);
+		pEngine->drawTexture(textureId, &middleOfScreen, hax::draw::Alignment::CENTER, widthTextureRect, heightTextureRect);
 	}
 
 	constexpr char TEXT[] = "EasyWinHax";
-	const float widthText = _countof(TEXT) * hax::draw::font::medium.width;
-	const float heightText = hax::draw::font::medium.height;
-	const hax::Vector2 bottomLeftText{ middleOfScreen.x - widthText / 2.f, middleOfScreen.y + heightText / 2.f };
+	constexpr uint32_t sizeText = 24u;
 	const hax::draw::Color colorText = argb ? hax::draw::argb::ORANGE : hax::draw::abgr::ORANGE;
-
-	pEngine->drawString(&hax::draw::font::medium, &bottomLeftText, TEXT, colorText);
+	pEngine->drawString(&middleOfScreen, hax::draw::Alignment::CENTER_LEFT, TEXT, sizeText, colorText);
 
 	return;
 }
