faa41368e95df140e18e488aff8afcb95673f6e5
belazr <benediktlazar92@gmail.com>
2025-05-29 12:21:42 +0200

    Deleted draw::SolidDrawBuffer::append overload

--- src/draw/SolidDrawBuffer.h
+++ src/draw/SolidDrawBuffer.h
@@ -31,23 +31,6 @@ namespace hax {
 			// Amount of vertices in the data array.
 			void append(const Vertex* data, uint32_t count);
 
-			// Appends vertices to the buffer.
-			//
-			// Parameters:
-			// 
-			// [in] data:
-			// Pointer to an array of screen coordinates of vertices to be appended to the buffer. Has to be called between beginFrame and endFrame calls.
-			//
-			// [in] count:
-			// Amount of vertices in the data array.
-			//
-			// [in] color:
-			// Color that the vertices will be drawn in. Color format: DirectX 9 -> argb, DirectX 10 -> abgr, DirectX 11 -> abgr, DirectX 12 -> abgr, OpenGL 2 -> abgr, Vulkan: application dependent
-			//
-			// [in] offset:
-			// Offset that gets added to all coordinates in the data array before the vertices get appended to the buffer.
-			void append(const Vector2* data, uint32_t count, Color color, Vector2 offset = { 0.f, 0.f });
-
 			// Ends the frame for the buffer and draws the contents. Has to be called after any append calls.
 			void endFrame() override;
 		
