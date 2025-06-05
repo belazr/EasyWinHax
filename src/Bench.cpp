4d63fc4bd93e7d6d87e37f397ea4df50e1ba8ea0
belazr <benediktlazar92@gmail.com>
2025-06-06 00:05:21 +0200

    Cleaned up hax::Bench

--- src/Bench.cpp
+++ src/Bench.cpp
@@ -6,17 +6,23 @@ namespace hax {
 	
 	Bench::Bench(const char* label, size_t runs): _label{ label }, _runs{ runs }, _startTime{}, _endTime{}, _measurements{} {
 		this->_measurements.reserve(this->_runs);
+
+		return;
 	}
 
 
 	void Bench::start() {
 		this->_startTime = std::chrono::duration<double>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+
+		return;
 	}
 
 
 	void Bench::end() {
 		this->_endTime = std::chrono::duration<double>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 		this->_measurements.append(this->_endTime - this->_startTime);
+
+		return;
 	}
 
 
@@ -34,6 +40,7 @@ namespace hax {
 			this->_measurements.resize(0u);
 		}
 
+		return;
 	}
 
 }
