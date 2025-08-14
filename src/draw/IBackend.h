2bbbd33b6ea42727703d8ce9e8509d87cc1f97d6
belazr <benediktlazar92@gmail.com>
2025-08-14 19:21:31 +0200

    Fixed error in IBackend documentation

--- src/draw/IBackend.h
+++ src/draw/IBackend.h
@@ -30,7 +30,7 @@ namespace hax {
 			// The argument of the the hooked function.
 			// For OpenGL 2 wglSwapBuffers hooks pass nothing.
 			// For DirectX 9 EndScene hooks pass nothing.
-			// For DirectX 10 EndScene hooks pass nothing.
+			// For DirectX 10 Present hooks pass nothing.
 			// For DirectX 11 Present hooks pass nothing.
 			// For DirectX 12 Present hooks pass the ID3D12CommandQueue* that was retrieved by dx12::getInitData().
 			// For Vulkan QueuePresentKHR hooks pass the VkDevice that was retrieved by vk::getInitData().
