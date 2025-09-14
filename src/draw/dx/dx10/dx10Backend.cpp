0706b2f1899ea824e40bcbcf064401c6527c1c7f
belazr <benediktlazar92@gmail.com>
2025-09-14 10:50:40 +0200

    Updated constant buffer on viewport top left change in dx10::Backend

--- src/draw/dx/dx10/dx10Backend.cpp
+++ src/draw/dx/dx10/dx10Backend.cpp
@@ -203,7 +203,7 @@ namespace hax {
 
 				if (!this->getViewport(&viewport)) return false;
 
-				if (viewport.Width != this->_viewport.Width || viewport.Height != this->_viewport.Height) {
+				if (this->viewportChanged(&viewport)) {
 					this->_viewport = viewport;
 
 					if (!this->updateConstantBuffer()) return false;
@@ -366,6 +366,14 @@ namespace hax {
 				}
 
 				return true;
+			}
+
+
+			bool Backend::viewportChanged(const D3D10_VIEWPORT* pViewport) const {
+				const bool topLeftChanged = pViewport->TopLeftX != this->_viewport.TopLeftX || pViewport->TopLeftY != this->_viewport.TopLeftY;
+				const bool dimensionChanged = pViewport->Width != this->_viewport.Width || pViewport->Height != this->_viewport.Height;
+
+				return topLeftChanged || dimensionChanged;
 			}
 
 
